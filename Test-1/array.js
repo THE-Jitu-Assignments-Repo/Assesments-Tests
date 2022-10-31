d1853d027c8d6b5590e6980b770806e5f84da8fa
mik284 <mikeykamau222@gmail.com>
2022-10-31 20:59:52 +0300

    Updated arrays methods

--- Test-1/array.js
+++ Test-1/array.js
@@ -9,16 +9,18 @@
 
 let  Narray = [1,2,4,7]
 
+// Method: 1
 // Narray.reduce((prev, curr)=> { 
 //     let sum =  + curr; 
 //     console.log(sum)}
 // )
 
 
+// Method:2
 // let sum = 0
 // let res = Narray.forEach(item=> sum +=item) 
 
-
+//method: 3
 // let sum = 0
 // for(let i=0; i < Narray.length; i++){
 //     sum+=Narray[i]   
